6aac57a6a859bf33bc141e1b8d93e65dffafed61
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-11-18 00:53:46 +0200

    add redux toolkit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toggleCompleted } from "redux/tasksSlice";
+import { getTasks } from "redux/selectors";
+
+
 const App = () => {
+  const value = useSelector(getTasks);
+  const dispatch = useDispatch();
+    
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ol>
+        {value.map(item=>{
+           return ( 
+             <li key={item.id}> 
+               <button type="button" onClick={()=>dispatch(toggleCompleted(item.id))}>{item.text}</button> 
+               {item.completed && <span>   done</span>} 
+             </li> 
+           ) 
+         })} 
+      </ol>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import App from 'components/App/App';
 import './index.css';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter basename="/goit-react-hw-06-phonebook">
       <App/>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
